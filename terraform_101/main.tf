2965eac4cf3b56b2293ce94f44e587a37f8d322a
Kashish <kashish.vaid29994@gmail.com>
2025-09-18 08:12:07 +0530

    terraform 101

---
+++ terraform_101/main.tf
@@ -0,0 +1,25 @@
+resource "aws_vpc" "production" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "production"
+  }
+}
+
+
+resource "aws_vpc" "dev" {
+  cidr_block = "10.10.0.0/16"
+  tags = {
+    Name = "Dev"
+  }
+
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.production.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
